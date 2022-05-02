9eaec8d9a680c6d2d8ee0789e137856cf5c2100a
RafaScripts <rafaelw@rafascripts.live>
2022-05-02 03:37:34 -0300

    update
    burger menu add

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,20 +1,79 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { slide as Menu } from 'react-burger-menu';
 import logo from '../../assets/Astronaut-and-Saturn-cartoon-illustration-vector-removebg-preview 1.png';
 import { FiAlignJustify, FiMonitor, FiPower } from 'react-icons/fi'
 import './styles.css';
+var styles = {
+    bmBurgerButton: {
+        position: 'fixed',
+        width: '36px',
+        height: '30px',
+        top: '36px'
+    },
+    bmMenuWrap: {
+        position: 'fixed',
+        height: '100%'
+    },
+    bmMenuWrap: {
+        position: 'fixed',
+        height: '100%',
+        left: '0px',
+        top: '103px'
+    },
+    bmMenu: {
+        background: '#a77dff',
+        margin: '0px',
+        padding: '0px',
+        fontSize: '18px',
+        fontWeight: 'bold'
+    },
+    bmItemList: {
+        color: '#b8b7ad',
+        padding: '3em'
+    },
+    bmOverlay: {
+        top: '16.4%',
+        left: '0px'
+    },
+}
+
+let items;
+
+items = [
+    <Link className='item' to='/' >Ponto de Venda</Link>,
+    <Link className='item' to='/orcamento' >Orçamentos</Link>,
+    <Link className='item' to='/' >Produtos</Link>,
+    <Link className='item' to='/' >Importar CSV</Link>,
+    <Link className='item' to='/' >Importar XML</Link>,
+    <Link className='item' to='/' >Usuarios</Link>,
+    <Link className='item' to='/' >Fiscal</Link>,
+    <Link className='item' to='/' >Relatorios</Link>
+
+]
 
 export default function Home() {
     return(
         <div className="list">
             <header>
-                <button className='button'><FiAlignJustify /></button>
+                <div id="outer-container">
+                    <Menu styles={styles} pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" }>
+                        {items}
+                    </Menu>
+                    <main id="page-wrap">
+                        <FiAlignJustify className='mmen' />
+                    </main>
+                </div>
                 <img src={logo} alt="Logo" />
                 <Link className="button" to="/product"><FiMonitor /> POS</Link>
-                <button type="button">
+                <button className='bb' type="button">
                     <FiPower size={18} color="#FFF" />
                 </button>
             </header>
+
+            <img className='backs' src={logo}/>
+
+            <h2>Em Desenvolvimento da tela Principal</h2>
         </div>
     );
 }
